aaface58975c7ed4528d48e5fc7bd76974cf80e2
phuockaito <huuphuocit1999@gmail.com>
2021-05-06 19:04:13 +0700

    Updates

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-
+  const scrollMenu = $('.ground-header');
   const btnShowSearch = $('.btn-show-search');
   const btnCloseSearch = $('.btn-search');
-  $("ul.menu").find('li.active-menu a').on('click', function () {
-    $(this).parent('li.active-menu').toggleClass('open');
-  });
-  const scrollMenu = $('.ground-header');
+  // $("ul.menu").find('li.active-menu a').on('click', function () {
+  //   $(this).parent('li.active-menu').toggleClass('open');
+  // });
   //
   btnShowSearch.on('click', function () {
     toggleShowSearch();
