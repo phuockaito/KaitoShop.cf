1d1731bec0bfd3e0a478fbb21dd4164ee064e69b
WE-SOLARBK\phuocth <huuphuocit1999>
2022-06-20 15:58:17 +0700

    http proxy

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosClient = axios.create({
-    baseURL: "https://kaito-shop.herokuapp.com/api",
+    baseURL: "/api",
     headers: {
         "content-type": "application/json",
     },
