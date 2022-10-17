c05fa2232a93f364dfcdcdd2f9cda1a67e0a8380
Trần Hữu Phước <57070629+phuockaito@users.noreply.github.com>
2022-10-17 22:54:08 +0700

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosClient = axios.create({
-    baseURL: "https://kaito-shop.herokuapp.com/api",
+    baseURL: "https://api-kaito-shop.vercel.app/api",
     headers: {
         "content-type": "application/json",
     },
