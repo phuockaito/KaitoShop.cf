1f2d666d40f88dbfb6e3af85012c36b765d5c190
Phuoc Kaito <huuphuocit1999@gmail.com>
2023-07-19 00:29:12 +0700

    fix: http

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -59,7 +59,7 @@ const UserContextProvider = ({ children }) => {
     // connect and get user if have token
     useEffect(() => {
         (async () => {
-            const socketIo = io("https://api-kaito-shop.vercel.app", {
+            const socketIo = io("ws://api-kaito-shop.vercel.app", {
                 // withCredentials: true,
                 autoConnect: true
 
